128be55960ddc8754cc7320fecc58450a528742c
alonzo ramirez <alonzocr1995@outlook.com>
2022-03-10 15:55:24 -0600

    Authentication done

---
+++ src/users/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with Id: ', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with Id: ', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with Id: ', this.id);
+  }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,92 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guards';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  // @Get('/whoami')
+  // async whoAmi(@Session() session: any) {
+  //   const user = await this.userService.findOne(session.userId);
+  // if (!user) {
+  //   throw new NotFoundException('User not found!');
+  // }
+
+  //   return user;
+  // }
+
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoamI(@CurrentUser() user: User) {
+    // if (!user) {
+    //   throw new NotFoundException('User not found!');
+    // }
+    return user;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  async signIn(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.sighin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not fuond!');
+    }
+    return user;
+  }
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+}
